b4a3d938e8bdd0dfdcdb5913ac63ea1115f72c31
vn-thaipt <thaipt@rakumo.vn>
2019-01-04 17:25:16 +0700

    Format code

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,26 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+	selector: 'app-heroes',
+	templateUrl: './heroes.component.html',
+	styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent {
+	heroes = HEROES;
+	selectedHero: Hero;
 
-  heroes = HEROES;
-  selectedHero: Hero;
-
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+	onSelect(hero: Hero): void {
+		this.selectedHero = hero;
+	}
 
 }
-
