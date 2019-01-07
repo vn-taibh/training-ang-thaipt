81c1271bd60936ce29c50c4496ce48022a742f87
vn-thaipt <thaipt@rakumo.vn>
2019-01-07 08:29:58 +0700

    Fix format code

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+
 import { Hero } from '../hero';
+
 import { HEROES } from '../mock-heroes';
 
 @Component({
@@ -8,11 +10,10 @@ import { HEROES } from '../mock-heroes';
 	styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-	heroes = HEROES;
+	heroes: Hero[] = HEROES;
 	selectedHero: Hero;
 
 	onSelect(hero: Hero): void {
 		this.selectedHero = hero;
 	}
-
 }
