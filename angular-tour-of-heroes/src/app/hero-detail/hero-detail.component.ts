886e9070366fa8e2bcfd021714db97650b70272f
vn-thaipt <thaipt@rakumo.vn>
2019-01-08 13:13:19 +0700

    Check format, HTTP, add, delete, update, search

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -37,4 +37,8 @@ export class HeroDetailComponent implements OnInit {
 		this.location.back();
 	}
 
+	save(): void {
+		this.heroService.updateHero(this.hero)
+		  .subscribe(() => this.goBack());
+	}
 }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,4 @@
-import { Component, 
-		OnInit 
-		} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
@@ -12,7 +10,6 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
 	heroes: Hero[] ;
-	selectedHero: Hero;
 
 	constructor(private heroService: HeroService) { }
 
@@ -20,12 +17,22 @@ export class HeroesComponent implements OnInit {
 		this.getHeroes();
 	}
 
-	onSelect(hero: Hero): void {
-		this.selectedHero = hero;
-	}
-
 	getHeroes(): void {
 		this.heroService.getHeroes()
 			.subscribe(heroes => this.heroes = heroes);
+	}
+
+	add(name: string): void {
+		name = name.trim();
+		if (!name) { return; }
+		this.heroService.addHero({ name } as Hero)
+			.subscribe(hero => {
+			this.heroes.push(hero);
+			});
+		}
+		
+	delete(hero: Hero): void {
+		this.heroes = this.heroes.filter(h => h !== hero);
+		this.heroService.deleteHero(hero).subscribe();
 	}
 }
