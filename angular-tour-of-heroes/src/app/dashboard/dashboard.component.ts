43d2a352380177f7fd2c6ba8d4152c44e0378642
vn-taibh <taibh@aoi-sys.vn>
2019-01-11 17:13:57 +0700

    Tweak dashboard component.

--- angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
+++ angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+	Component,
+	OnInit,
+	OnDestroy
+} from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Subscription } from 'rxjs';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
@@ -8,17 +15,32 @@ import { HeroService } from '../hero.service';
 	templateUrl: './dashboard.component.html',
 	styleUrls: [ './dashboard.component.css' ]
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
 	heroes: Hero[] = [];
+	subscription: Subscription;
  
-	constructor(private heroService: HeroService) { }
+	constructor(
+		private heroService: HeroService,
+		private router: Router
+	) { }
  
 	ngOnInit() {
 		this.getHeroes();
 	}
  
 	getHeroes(): void {
-		this.heroService.getHeroes()
-			.subscribe(heroes => this.heroes = heroes.slice(1, 5));
+		this.subscription = this.heroService.getHeroes().subscribe(
+			heroes => this.heroes = heroes.slice(1, 5)
+		);
+	}
+
+	getHeroDetail(id: string): void {
+		this.router.navigateByUrl(`/detail/${id}`);
+	}
+
+	ngOnDestroy(): void {
+		if (this.subscription) {
+			this.subscription.unsubscribe();
+		}
 	}
 }
